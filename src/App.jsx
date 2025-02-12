4de988e444f023823c2efc5283376b671fc8eb0f
Prahalad Singh Gaur <singhgaurprahalad@gmail.com>
2025-02-12 12:21:21 +0530

    create App.jsx and write basic info

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h1>Your Name</h1>
+          <p>Contact: your.email@example.com</p>
+          <p>Phone: +91 98765XXXXX</p>
+      </div>
+  );
+}
+
+export default App;
+
